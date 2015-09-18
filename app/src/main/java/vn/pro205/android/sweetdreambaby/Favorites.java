19638861dfbdb529bcc0ab7d166201819a94f0af
nguyenkhoido <nguyendokhoi35@gmail.com>
2015-09-18 17:07:00 +0700

    Update Favorites List

--- app/src/main/java/vn/pro205/android/sweetdreambaby/Favorites.java
+++ app/src/main/java/vn/pro205/android/sweetdreambaby/Favorites.java
@@ -5,9 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import vn.pro205.android.sweetdreambaby.Adapters.FavoriteBooksAdapter;
-
-public class Favorites extends android.app.ListFragment {
+public class Favorites extends android.app.Fragment {
 
     public Favorites() {
         // Required empty public constructor
@@ -17,17 +15,28 @@ public class Favorites extends android.app.ListFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment//
         //aaa
-        String[] titleBook = {"Title Story", "Title Story", "Title Story", "Title Story", "Title Story", "Title Story", "Title Story", "Title Story", "Title Story", "Title Story"};
+        /*String[] titleBook = {"Title Story", "Title Story", "Title Story", "Title Story", "Title Story", "Title Story", "Title Story", "Title Story", "Title Story", "Title Story"};
         String [] typeBook = {"Free","49.000vnd","Free","49.000vnd","Free","Free","39.000vnd","Free","59.000vnd","Free"};
         int[] image = {R.drawable.lordofring, R.drawable.lordofring, R.drawable.xmenfirstclass, R.drawable.xmenfirstclass, R.drawable.lordofring,
                 R.drawable.ironman2, R.drawable.thor, R.drawable.ironman2, R.drawable.xmenfirstclass, R.drawable.thor};
         FavoriteBooksAdapter adapter = new FavoriteBooksAdapter(getActivity(),titleBook,typeBook,image);
-        setListAdapter(adapter);
+        setListAdapter(adapter);*/
 
         View view = inflater.inflate(R.layout.activity_favorites, container, false);
 
+
+            //setContentView(R.layout.activity_settings);
+
+           /* Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);*/
+
+           /* CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) view.findViewById(R.id.collapsing_toolbar);
+            collapsingToolbar.setTitle("Second Activity");*/
+
         return view;
     }
+
 
 
 }
